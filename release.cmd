2f101b32263e513043452c3d82de70f1b1c71c98
JustOff <Off.Just.Off@gmail.com>
2020-02-03 23:03:00 +0200

    Bump version

--- release.cmd
+++ release.cmd
@@ -1,5 +1,5 @@
 @echo off
-set VER=1.0.2
+set VER=1.1.0
 
 sed -i -E "s/version>.+?</version>%VER%</" classic\install.rdf
 sed -i -E "s/version>.+?</version>%VER%</; s/download\/.+?\/(.+?)classic-.+?\.xpi/download\/%VER%\/\1classic-%VER%\.xpi/" update.xml
